2f63118fc2dd412ca9f06dbececbf279151e947b
yshuangze <3309243468@qq.com>
2023-01-02 20:15:01 +0800

    first commit

---
+++ src/main/java/com/test/GitTest.java
@@ -0,0 +1,17 @@
+package com.test;
+
+/**
+ * com.test
+ * yang'shuang'ze
+ * 2023/1/2
+ * 20:08
+ *
+ * @author yang'shuang'ze
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+
+    }
+}
